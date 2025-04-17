7a8843ae65d247ebdd9fbbecb3d4eb49983837be
harvey <alstjr971@naver.com>
2025-04-18 03:38:27 +0900

    first commit

---
+++ src/test/java/inflearn/springadvance/SpringAdvanceApplicationTests.java
@@ -0,0 +1,13 @@
+package inflearn.springadvance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdvanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
