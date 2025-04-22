2519bf4d41bef42aec0e14efaa77336e096e4afd
harvey <alstjr971@naver.com>
2025-04-22 21:50:53 +0900

    feat: 로그 추적기 3-Tier에 적용
    [11050632] inflearn.springadvance.v1.OrderControllerV1 request()
    [f88b5fe8] inflearn.springadvance.v1.OrderServiceV1 request()
    [1d26d2ca] inflearn.springadvance.v1.OrderRepositoryV1 request()
    [1d26d2ca] inflearn.springadvance.v1.OrderRepositoryV1 request() time=1010ms
    [f88b5fe8] inflearn.springadvance.v1.OrderServiceV1 request() time=1012ms
    [11050632] inflearn.springadvance.v1.OrderControllerV1 request() time=1013ms

---
+++ src/main/java/inflearn/springadvance/v1/OrderControllerV1.java
@@ -0,0 +1,31 @@
+package inflearn.springadvance.v1;
+
+import inflearn.springadvance.hellotrace.HelloTraceV1;
+import inflearn.springadvance.trace.TraceStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV1 {
+
+    private final OrderServiceV1 orderService;
+    private final HelloTraceV1 trace;
+
+    @GetMapping("/v1/request")
+    public String request(@RequestParam(required = false) String itemId) {
+
+        TraceStatus status = null;
+        try {
+            status = trace.begin(this.getClass().getName() + " " + "request()");
+            orderService.orderItem(itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; // 예외를 꼭 다시 던져주어야 함
+        }
+    }
+}
---
+++ src/main/java/inflearn/springadvance/v1/OrderRepositoryV1.java
@@ -0,0 +1,45 @@
+package inflearn.springadvance.v1;
+
+import inflearn.springadvance.hellotrace.HelloTraceV1;
+import inflearn.springadvance.trace.TraceStatus;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV1 {
+
+    private final HelloTraceV1 trace;
+
+    public void save(String itemId) {
+
+        TraceStatus status = null;
+        try {
+            status = trace.begin(this.getClass().getName() + " " + "request()");
+
+            //save logic
+            if (itemId.equals("ex")) {
+                throw new IllegalStateException("Exception Thrown");
+            }
+            sleep(1000);
+
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; // 예외를 꼭 다시 던져주어야 함
+        }
+
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
