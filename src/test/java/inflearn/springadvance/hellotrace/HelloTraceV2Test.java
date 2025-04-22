7a6191ef17b703e1b16623e9f9c7a0452c0ffe56
harvey <alstjr971@naver.com>
2025-04-22 22:05:55 +0900

    test: TraceV2 level 동기화 테스트

---
+++ src/test/java/inflearn/springadvance/hellotrace/HelloTraceV2Test.java
@@ -0,0 +1,27 @@
+package inflearn.springadvance.hellotrace;
+
+import inflearn.springadvance.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+class HelloTraceV2Test {
+
+    @Test
+    void begin_end(){
+        HelloTraceV2 trace = new HelloTraceV2();
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+
+        trace.end(status2);
+        trace.end(status1);
+    }
+
+    @Test
+    void begin_ex() {
+        HelloTraceV2 trace = new HelloTraceV2();
+        TraceStatus status1 = trace.begin("ex hello1");
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(), "ex hello2");
+        trace.exception(status2, new IllegalStateException());
+        trace.exception(status1, new IllegalStateException());
+    }
+
+}
