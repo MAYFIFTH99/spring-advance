23b5dad2b2393569aef805724d3257589ab7c4ac
harvey <alstjr971@naver.com>
2025-06-17 21:22:46 +0900

    test: FieldLogTrace 기능 테스트

---
+++ src/test/java/inflearn/springadvance/trace/logtrace/FieldLogTraceTest.java
@@ -0,0 +1,26 @@
+package inflearn.springadvance.trace.logtrace;
+
+import inflearn.springadvance.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+class FieldLogTraceTest {
+
+    FieldLogTrace trace = new FieldLogTrace();
+
+    @Test
+    void begin_end_level2() {
+
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.begin("hello2");
+        trace.end(status2);
+        trace.end(status1);
+    }
+
+    @Test
+    void begin_ex_level2() throws Exception {
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.begin("hello2");
+        trace.exception(status2, new IllegalArgumentException("예외 발생!"));
+        trace.end(status1);
+    }
+}
