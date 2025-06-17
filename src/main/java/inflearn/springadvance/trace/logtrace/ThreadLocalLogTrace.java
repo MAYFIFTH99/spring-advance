daa9be2c09030f6dc08f7416ca931029d1c13884
harvey <alstjr971@naver.com>
2025-06-17 22:59:29 +0900

    feat: ThreadLocal 기반 LogTrace 구현 적용 (OCP & Strategy 패턴 적용)
    - LogTrace 인터페이스에 ThreadLocal 기반 구현체 적용
    - OCP 원칙을 따르며 클라이언트 코드 수정 없이 구현체 교체
    - Strategy 패턴 구성으로 스프링 빈 등록 방식으로 유연하게 적용

---
+++ src/main/java/inflearn/springadvance/trace/logtrace/ThreadLocalLogTrace.java
@@ -0,0 +1,78 @@
+package inflearn.springadvance.trace.logtrace;
+
+import inflearn.springadvance.trace.TraceId;
+import inflearn.springadvance.trace.TraceStatus;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ThreadLocalLogTrace implements LogTrace{
+
+    private static final String START_PREFIX = "-->";
+    private static final String COMPLETE_PREFIX = "<--";
+    private static final String EX_PREFIX = "<X-";
+
+//    private TraceId traceIdHolder; // traceId 동기화, 동시성 이슈 존재
+    private ThreadLocal<TraceId> threadLocalTraceId = new ThreadLocal<>();
+
+    @Override
+    public TraceStatus begin(String message) {
+        syncTraceId(); // traceId 동기화
+        TraceId traceId = threadLocalTraceId.get(); // 현재 traceId 가져오기
+        long startTimeMs = System.currentTimeMillis();
+        log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX, traceId.getLevel()), message);
+        return new TraceStatus(traceId, startTimeMs, message);
+    }
+
+    private void syncTraceId(){
+        TraceId traceId = threadLocalTraceId.get();
+        if (traceId == null) {
+            threadLocalTraceId.set(new TraceId());
+        } else {
+            threadLocalTraceId.set(traceId.createNextLevel());
+        }
+    }
+
+    @Override
+    public void end(TraceStatus status) {
+        complete(status, null);
+    }
+
+    @Override
+    public void exception(TraceStatus status, Exception e) {
+        complete(status, e);
+    }
+
+    private void complete(TraceStatus status, Exception e) {
+        Long stopTimeMs = System.currentTimeMillis();
+        long resultTimeMs = stopTimeMs - status.getStartTimeMs();
+        TraceId traceId = status.getTraceId();
+
+        if (e == null) {
+            log.info("[{}] {}{} time={}ms", traceId.getId(), addSpace(COMPLETE_PREFIX,
+                    traceId.getLevel()), status.getMessage(), resultTimeMs);
+        } else {
+            log.info("[{}] {}{} time={}ms ex={}", traceId.getId(), addSpace(EX_PREFIX,
+                    traceId.getLevel()), status.getMessage(), resultTimeMs, e.toString());
+        }
+
+        releaseTraceId(); // traceId 레벨 감소
+    }
+
+    private void releaseTraceId() {
+        TraceId traceId = threadLocalTraceId.get();
+        if( traceId.isFirstLevel()) {
+            threadLocalTraceId.remove(); // 최상위 레벨로 돌아오면 traceIdHolder 초기화
+        } else {
+            threadLocalTraceId.set(traceId.createPreviousLevel()); // 레벨 감소
+        }
+    }
+
+    private static String addSpace(String prefix, int level) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < level; i++) {
+            sb.append((i == level - 1) ? "|" + prefix : "|  ");
+        }
+        return sb.toString();
+    }
+}
