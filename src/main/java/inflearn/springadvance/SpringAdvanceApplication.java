05b6621e787f7646dbed12bda531e96e80f4900b
harvey <alstjr971@naver.com>
2025-06-18 22:20:29 +0900

    feat: proxy V3 - 구현체, 빈 자동 등록

--- src/main/java/inflearn/springadvance/SpringAdvanceApplication.java
+++ src/main/java/inflearn/springadvance/SpringAdvanceApplication.java
@@ -1,11 +1,15 @@
 package inflearn.springadvance;
 
+import inflearn.springadvance.proxy.config.AppV1Config;
+import inflearn.springadvance.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "inflearn.springadvance.proxy")
+@Import({AppV1Config.class, AppV2Config.class})
+@ComponentScan(basePackages = "inflearn.springadvance.proxy.app")
 public class SpringAdvanceApplication {
 
     public static void main(String[] args) {
--- src/main/java/inflearn/springadvance/proxy/app/v2/OrderServiceV2.java
+++ src/main/java/inflearn/springadvance/proxy/app/v2/OrderServiceV2.java
@@ -1,7 +1,5 @@
 package inflearn.springadvance.proxy.app.v2;
 
-import inflearn.springadvance.proxy.app.v1.OrderRepositoryV1;
-
 public class OrderServiceV2 {
 
     private final OrderRepositoryV2 orderRepository;
