91aea236b4efd0ed511e09a47b8fe2bf39c8e55a
harvey <alstjr971@naver.com>
2025-06-18 13:02:59 +0900

    docs: 주석 추가 - 템플릿 콜백 패턴
    - 템플릿 콜백 패턴은 전략 패턴에 템플릿과 콜백이 강조된 패턴이다.

--- src/test/java/inflearn/springadvance/trace/strategy/code/ContextV2.java
+++ src/test/java/inflearn/springadvance/trace/strategy/code/ContextV2.java
@@ -13,7 +13,12 @@ public class ContextV2 {
         long startTime = System.currentTimeMillis();
 
         //비즈니스 로직 실행
-        strategy.call();
+        strategy.call(); // 이렇게 다른 코드의 인수로 넘겨서 실행 가능한 코드를 `콜백`이라 한다.
+        /**
+         * 템플릿-콜백 패턴은 GOF 패턴 중 하나가 아니고, 스프링 내부에서 이런 방식을 자주 사용하기 때문에
+         * 스프링 안에서만 이렇게 부른다.
+         * 정리하면, 템플릿-콜백 배턴은 `전략 패턴`에서 '템플릿'과 '콜백' 부분이 강조된 패턴이라 생각하면 된다.
+         */
         //비즈니스 로직 종료
 
         long endTime = System.currentTimeMillis();
