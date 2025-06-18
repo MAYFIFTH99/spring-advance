227dbf290261c89ec33225819cf265b7eead2399
harvey <alstjr971@naver.com>
2025-06-18 09:29:01 +0900

    test: 템플릿 메서드 패턴 예제 테스트

---
+++ src/test/java/inflearn/springadvance/trace/template/TemplateMethodTest.java
@@ -0,0 +1,52 @@
+package inflearn.springadvance.trace.template;
+
+import inflearn.springadvance.trace.template.code.SubClassLogic1;
+import inflearn.springadvance.trace.template.code.SubClassLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TemplateMethodTest {
+
+    @Test
+    void templateMethodV0() throws Exception {
+        logic1();
+        logic2();
+    }
+
+    private void logic1() {
+        long startTime = System.currentTimeMillis();
+
+        //비즈니스 로직 실행
+        log.info("비즈니스 로직1 실행");
+        //비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+
+    }
+
+    @Test
+    void logic2()  {
+        long startTime = System.currentTimeMillis();
+
+        //비즈니스 로직 실행
+        log.info("비즈니스 로직2 실행");
+        //비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+
+    @Test
+    void templateMethodV1() {
+        SubClassLogic1 logic1 = new SubClassLogic1();
+        logic1.execute();
+
+        SubClassLogic2 logic2 = new SubClassLogic2();
+        logic2.execute();
+    }
+}
