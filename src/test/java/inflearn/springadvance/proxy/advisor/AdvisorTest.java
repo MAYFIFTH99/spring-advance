9fae63ec6ee8dfac4a5bc075bc3d56a7d04b11f4
harvey <alstjr971@naver.com>
2025-06-20 15:23:41 +0900

    test: 어드바이저 test 1

---
+++ src/test/java/inflearn/springadvance/proxy/advisor/AdvisorTest.java
@@ -0,0 +1,29 @@
+package inflearn.springadvance.proxy.advisor;
+
+import inflearn.springadvance.proxy.common.advice.TimeAdvice;
+import inflearn.springadvance.proxy.common.service.ServiceImpl;
+import inflearn.springadvance.proxy.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+@Slf4j
+public class AdvisorTest {
+
+    @Test
+    void adviserTest1() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE,
+                new TimeAdvice());
+
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.find();
+        proxy.save();
+    }
+}
